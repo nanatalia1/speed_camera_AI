463ead96ebe222bb8076331ced88a731724c665e
Natalia Czapla <126421760+nanatalia1@users.noreply.github.com>
2023-05-26 18:21:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,109 @@
-from PIL import Image
 import cv2
-import requests
-import numpy as np
-import icrawler
+import imutils as imutils
+import pytesseract as pytesseract
+from ultralytics import YOLO
 from cascadeutils import generate_negative_description_file
 
-#generate_negative_description_file()
-#cascade_limestone= cv.CascadeClassifier('cascade/cascade.xml')
-image = Image.open(r"C:\Users\48516\PycharmProjects\AIproject\positive\17.jpg")
-image = image.resize((450, 250))
-image_arr=np.array(image)
-#image.show()
 
-grey = cv2.cvtColor(image_arr,cv2.COLOR_BGR2GRAY)
-Image.fromarray(grey)
+#wczytanie
+image = cv2.imread(r"C:\Users\48516\Downloads\2021-Nissan-Qashqai-front-view-1200x800.jpg")
+car_cascade = cv2.CascadeClassifier('cascade/cascade.xml')
+model = YOLO("yolov8m.pt")
 
-blur = cv2.GaussianBlur(grey,(5,5),0)
-Image.fromarray(blur)
+def processPlate(image=image):
+    image = imutils.resize(image, width=500)
 
-dilated = cv2.dilate(blur,np.ones((3,3)))
-Image.fromarray(dilated)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("greyed image", gray_image)
+    cv2.waitKey(0)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
-Image.fromarray(closing)
+    gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
+    cv2.imshow("smoothened image", gray_image)
+    cv2.waitKey(0)
 
-car_cascade_src = 'cascade/cascade.xml'
-car_cascade = cv2.CascadeClassifier(car_cascade_src)
-cars = car_cascade.detectMultiScale(closing, 1.1, 1)
-cars
+    edged = cv2.Canny(gray_image, 30, 200)
+    cv2.imshow("edged image", edged)
+    cv2.waitKey(0)
 
-cnt = 0
-for (x,y,w,h) in cars:
-    cv2.rectangle(image_arr,(x,y),(x+w,y+h),(255,0,0),2)
-    cnt += 1
-print(cnt, " cars found")
-Image.fromarray(image_arr).show()
+    cnts, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    image1 = image.copy()
+    cv2.drawContours(image1, cnts, -1, (0, 255, 0), 3)
+    cv2.imshow("contours", image1)
+    cv2.waitKey(0)
+
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:30]
+    screenCnt = None
+    image2 = image.copy()
+    cv2.drawContours(image2, cnts, -1, (0, 255, 0), 3)
+    cv2.imshow("Top 30 contours", image2)
+    cv2.waitKey(0)
+
+    i = 7
+    for c in cnts:
+        perimeter = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            x, y, w, h = cv2.boundingRect(c)
+            new_img = image[y:y + h, x:x + w]
+            cv2.imwrite('./' + str(i) + '.png', new_img)
+            i += 1
+            break
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+    cv2.imshow("image with detected license plate", image)
+    cv2.waitKey(0)
+
+    Cropped_loc = './7.png'
+    cv2.imshow("cropped", cv2.imread(Cropped_loc))
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    plate = pytesseract.image_to_string(Cropped_loc, lang='eng')
+    print("Number plate is:", plate)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
+def carsDetectionHAAR():
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if len(cars) > 0:
+
+
+        # Znajdź największy ROI
+        max_area = 0
+        max_roi = None
+
+        for (x, y, w, h) in cars:
+            area = w * h
+            if area > max_area:
+                max_area = area
+                max_roi = (x, y, w, h)
+
+        if max_roi is not None:
+            # Pobierz współrzędne największego ROI
+            x, y, w, h = max_roi
+
+            # Wyciągnij ROI na podstawie współrzędnych ramki ograniczającej
+            roi = image[y:y + h, x:x + w]
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Wyświetl ROI
+            cv2.imshow('ROI - Największy wykryty samochód', roi)
+            cv2.imshow('Obraz z ROI', image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            processPlate(roi)
+        else:
+            print("Nie można znaleźć ROI samochodu!")
+    else:
+        print("Nie wykryto samochodów na zdjęciu!")
+
+
+def carsDetectionYOLO():
+    results = model.predict(image)
+    result = results[0]
+    print(result.names)
+
+carsDetectionYOLO()
+#carsDetectionHAAR()
+#processPlate(image)
